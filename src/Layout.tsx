f633651f03d181dff3574e71865ba0ee96e14fc3
tomos <oerushia@gmail.com>
2023-04-13 23:30:49 +0100

    center nav

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -6,7 +6,7 @@ function Layout() {
 
     return (
         <div className={theme + " w-screen h-screen"}>
-            <nav className="h-[5%] bg-crust text-center space-x-6 sm:space-x-6 md:space-x-8 lg:space-x-16 leading-10 items-center">
+            <nav className="h-[5%] pt-[0.5%] bg-crust text-center space-x-6 sm:space-x-6 md:space-x-8 lg:space-x-16">
                 <NavLink to="/" className="text-lavender hover:text-blue">Home</NavLink>
                 <NavLink to="languages" className="text-lavender hover:text-blue">Languages</NavLink>
                 <NavLink to="games" className="text-lavender hover:text-blue">Games</NavLink>
